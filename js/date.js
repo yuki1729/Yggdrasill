8a2c65e77ace5c58bf71135e44a564d199ce8822
yuki1729 <yuki.o1729@gmail.com>
2016-03-19 14:29:02 +0900

    正規表現をjavascript上で認識できる形に記載

--- js/date.js
+++ js/date.js
@@ -8,7 +8,7 @@ document.getElementById('limit').onsubmit = function(){
 //    date.replace(/[０-９]/g,function(s){return String.fromCharCode(s.charCodeAt(0)-65248);});
 
 //入力された数字が1桁または2桁だった場合は，日付と認識してその時点の日と比較した上でその時点の年と月を補完
-    if (date === ^([1-9]|[12][0-9]|3[01])$) {
+    if (date === /^([1-9]|[12][0-9]|3[01])$/) {
       if (date < realdate) {
         month = month +1;
       }
@@ -18,8 +18,8 @@ document.getElementById('limit').onsubmit = function(){
   return false;
     }
 // 入力された数字が3桁または4桁だった場合は，月日と認識して年を補完
-    else if (date === ^([1-9][1[0-2])([1-9]|[12][0-9]|3[01])$) {
-      if (month === 1||2||3||4||5||6||7||8||9) {
+    else if (date === /^([1-9][1[0-2])([1-9]|[12][0-9]|3[01])$/) {
+      if (month === /[1-9]/) {
         month = "0" + month
       }
       date = year + date.substring(0,2) + date.substring(2,4)
@@ -37,14 +37,14 @@ document.getElementById('limit').onsubmit = function(){
 }
 */
 //入力された数字が5桁または6桁だった場合は，頭に20をつけてフル西暦にした上で，3桁と同じ処理。3000年以降はどうする？
-     else if(date === ^(([1-9][0-9])([1-9][1[0-2])([1-9]|[12][0-9]|3[01])$) {
+     else if(date === /^(([1-9][0-9])([1-9][1[0-2])([1-9]|[12][0-9]|3[01])$/) {
       date = '20' + date.substr(0,2) + date.substr(2,6)
       var date2 = moment(date).format("YYYY/MM/DD");
   document.getElementById('limit').textContent = date2;
   return false;
 }
 //入力された数字が7桁または8桁だった場合
-    else if (date === ^([1-3][0-9][0-9][0-9][[1-9]|1[0-2][1-9]|[12][0-9]|3[01])$) {
+    else if (date === /^([1-3][0-9][0-9][0-9][[1-9]|1[0-2][1-9]|[12][0-9]|3[01])$/) {
       var date = moment(date).format("YYYY/MM/DD");
       document.getElementById('limit').textContent = date;
       return false;
