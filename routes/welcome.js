12d48fd4afbd2df5b9ad8fdb1510f0d14aefecba
igaki <gymtaka@gmail.com>
2017-01-02 19:38:42 +0900

    ハッシュ値に変換はできたけどSQL文が通らない

--- routes/welcome.js
+++ routes/welcome.js
@@ -2,20 +2,30 @@ var express = require('express');
 var router = express.Router();
 var moment = require('moment');
 var connection = require('../mysqlConnection'); // è¿½åŠ
+var crypto = require('crypto');
 
 router.post('/', function(req, res, next) {
   var register_username = '"' + req.body.username + '", ';
   var register_password = '"' + req.body.password + '", ';
+//register_passwordの暗号化
+　var cipher = crypto.createCipher('aes-256-cbc',register_password);
+  var crypto_password = cipher.update(register_password, 'utf8', 'hex');
+  crypto_password += '"' + cipher.final('hex') + '", ';
+
+ console.log('passwordtest' + crypto_password);
+
   // var register_mail = '"' + req.body.mail + '", ';
   console.log("-------------------post-------------------")
   console.log(req.body)
   var query =
-    'INSERT INTO user (user_name, password, mail) VALUES ('
+    'INSERT INTO user (user_name, password, mail, created_at) VALUES ('
     + register_username
-    + register_password
-    + '"testtest@mail.com"'
+    + crypto_password
+    + 'testtest@mail.com,'
+    + '2016-12-30'
     + ')';
 
+    //
   connection.query(query, function(err, rows) {
     res.redirect('/welcome');
   });
@@ -32,3 +42,4 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+exports.welcome = require('./welcome');
