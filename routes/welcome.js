b5c6be46da386d3579d3f7a4824d3c6247c4f207
igaki <gymtaka@gmail.com>
2017-01-02 21:14:24 +0900

    IDとパスワードをDBに登録成功

--- routes/welcome.js
+++ routes/welcome.js
@@ -5,27 +5,32 @@ var connection = require('../mysqlConnection'); // è¿½åŠ
 var crypto = require('crypto');
 
 router.post('/', function(req, res, next) {
-  var register_username = '"' + req.body.username + '", ';
-  var register_password = '"' + req.body.password + '", ';
+  var register_username = req.body.username;
+  var register_password = req.body.password;
 //register_passwordの暗号化
 　var cipher = crypto.createCipher('aes-256-cbc',register_password);
   var crypto_password = cipher.update(register_password, 'utf8', 'hex');
-  crypto_password += '"' + cipher.final('hex') + '", ';
+  crypto_password +=  cipher.final('hex') ;
 
  console.log('passwordtest' + crypto_password);
 
-  // var register_mail = '"' + req.body.mail + '", ';
+  var register_mail = req.body.mail;
   console.log("-------------------post-------------------")
   console.log(req.body)
   var query =
     'INSERT INTO user (user_name, password, mail, created_at) VALUES ('
-    + register_username
-    + crypto_password
-    + 'testtest@mail.com,'
-    + '2016-12-30'
+    + '"' + register_username + '" , '
+    + '"' + crypto_password + '" , '
+    + '"' + register_mail + '" , '
+    + 'NOW() '
     + ')';
 
-    //
+//MySQLで通ったSQL文
+//SELECT * FROM mydb.user;
+//INSERT INTO user (user_name, password, mail, created_at) VALUES (
+//'register_username' , 'crypto_password' , 'testtest@mail.com' , '2016-12-10 17:11:34');
+
+
   connection.query(query, function(err, rows) {
     res.redirect('/welcome');
   });
