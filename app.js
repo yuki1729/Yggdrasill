b270b310a63f80a07c777cef02ae6542ce02a49d
igaki <gymtaka@gmail.com>
2017-01-02 21:24:01 +0900

    loginとwelcomeにページを分割

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var welcome = require('./routes/welcome');
+var login = require('./routes/login');
+
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/welcome', welcome);
+app.use('/login', login);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
