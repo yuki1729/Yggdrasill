94ab3ad333cae8fe8ddc84a0127711d65e25e1a9
igaki <gymtaka@gmail.com>
2017-01-09 18:27:09 +0900

    IDとパスワードの判定成功。マージ先間違えたので再送

--- routes/login.js
+++ routes/login.js
@@ -8,38 +8,38 @@ router.post('/', function(req, res, next) {
   var register_username = req.body.username;
   var register_mail = req.body.mail;
   var register_password = req.body.password;
-  var emailExistsQuery = 'SELECT * FROM mydb.user WHERE user_name = "' + register_username + '" LIMIT 1';
-
 
 //register_passwordの暗号化
 　var cipher = crypto.createCipher('aes-256-cbc',register_password);
   var crypto_password = cipher.update(register_password, 'utf8', 'hex');
   crypto_password +=  cipher.final('hex') ;
 
- console.log('passwordtest' + crypto_password);
+  var query = 'SELECT id FROM mydb.user WHERE user_name = "' + register_username + '" AND password = "' + crypto_password + '" LIMIT 1';
 
-
-  console.log("-------------------post-------------------")
-  console.log(req.body)
-
-/*
-  var query =
-    'INSERT INTO user (user_name, password, mail, created_at) VALUES ('
-    + '"' + register_username + '" , '
-    + '"' + crypto_password + '" , '
-    + '"' + register_mail + '" , '
-    + 'NOW() '
-    + ')';
+/*MySQLで動くSQL文
+SELECT * FROM mydb.user;
+SELECT user_name FROM mydb.user WHERE user_name = 'igaki' AND password =  '7e953ce6ce197ab31f16facdc3f403f4' LIMIT 1;
 */
-
-//MySQLで通ったSQL文
-//SELECT * FROM mydb.user;
-//INSERT INTO user (user_name, password, mail, created_at) VALUES (
-//'register_username' , 'crypto_password' , 'testtest@mail.com' , NOW());
-
 
   req.session.user_id = register_username;
   console.log("session user id: " + req.session.user_id);
+
+connection.query(query, function(err, rows) {
+  var userId = rows.length? rows[0].id: false;
+  console.log(userId + '**************');
+  if (userId) {
+    req.session.user_id = userId;
+    res.redirect('/');
+  } else {
+    res.render('login', {
+      title: 'ログイン',
+      noUser: 'メールアドレスとパスワードが一致するユーザーはいません'
+    });
+  }
+ });
+
+
+
 
   router.get('/', function(req, res, next) {
     if(req.session.user_id){
@@ -51,19 +51,19 @@ router.post('/', function(req, res, next) {
   }
 });
 
-
+/*
   connection.query(emailExistsQuery, function(err, mail) {
     var emailExists = mail.length === 1;
     if (emailExists) {
-      res.redirect('/');
+      res.render('login', {
+        title: 'ログイン',
+        emailExists: '登録されていないメールアドレスです。'
+      });
     } else {
-    res.render('login', {
-      title: 'ログイン',
-      emailExists: '登録されていないメールアドレスです。'
-    });
+      res.redirect('/');
     }
   });
-
+*/
 /*
   connection.query(query, function(err, rows) {
     res.redirect('/login');
