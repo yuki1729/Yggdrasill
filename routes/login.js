f46ad97c7d695e1de24446f96dff4ae93ac6b08a
igaki <gymtaka@gmail.com>
2017-01-18 20:13:04 +0900

    バグつぶし完了

--- routes/login.js
+++ routes/login.js
@@ -6,7 +6,7 @@ var crypto = require('crypto');
 
 router.post('/', function(req, res, next) {
   var register_username = req.body.username;
-  var register_mail = req.body.mail;
+  var register_mail = req.body.username;
   var register_password = req.body.password;
 
   //register_passwordの暗号化
@@ -30,49 +30,53 @@ router.post('/', function(req, res, next) {
     if (userId) {
       req.session.user_id = userId;
       res.redirect('/');
+      return;
     } else {
       res.render('login', {
         title: 'ログイン',
         noUser: 'メールアドレスとパスワードが一致するユーザーはいません'
       });
     }
+
+    var getUserIdQuery = 'SELECT id FROM mydb.user WHERE user_name = "' + register_username + '" LIMIT 1';
+    connection.query(getUserIdQuery, function(err, result) {
+      var sessionUserId =  result[0].id;
+      req.session.user_id = sessionUserId;
+      console.log("---sessionUserId---")
+      console.dir(result);
+      console.log(result[0].id);
+
+
+      var emailExistsQuery = 'SELECT * FROM mydb.user WHERE id = "' + sessionUserId + '" LIMIT 1';
+      connection.query(emailExistsQuery, function(err, mail) {
+        var emailExists = mail.length === 1;
+        if (emailExists) {
+          console.log(emailExists + 'errrrrrrrrrrrrrrrrrrrrr');
+
+          res.redirect('/');
+          return;
+        } else {
+    //      connection.query(query, function(err, rows) {
+    //      });
+        res.render('login', {
+          title: 'ログイン',
+          emailExists: 'セッションユーザー ID' + sessionUserId + 'です'
+        });
+
+        }
+      });
+
+
+  });
+
+  });
+
    });
 
 
-  var getUserIdQuery = 'SELECT id FROM mydb.user WHERE user_name = "' + register_username + '" LIMIT 1';
-  connection.query(getUserIdQuery, function(err, result) {
-    var sessionUserId =  result[0].id;
-    req.session.user_id = sessionUserId;
-    console.log("---sessionUserId---")
-    console.dir(result);
-    console.log(result[0].id);
-    var emailExistsQuery = 'SELECT * FROM mydb.user WHERE id = "' + sessionUserId + '" LIMIT 1';
-    connection.query(emailExistsQuery, function(err, mail) {
-      var emailExists = mail.length === 1;
-      if (emailExists) {
-        res.redirect('/');
-
-      } else {
-  //      connection.query(query, function(err, rows) {
-  //      });
-      res.render('login', {
-        title: 'ログイン',
-        emailExists: 'セッションユーザー ID' + sessionUserId + 'です'
-      });
-
-      }
-    });
-  });
-
-
-
-
-
-
-});
-
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('login',
   { title: 'login',
