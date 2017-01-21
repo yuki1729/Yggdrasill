4f8429796c17c82669eb5be3666cd144c80cd207
yuki1729 <yuki.o1729@gmail.com>
2017-01-21 19:12:11 +0900

    タスク登録省スペース化完了

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,4 +1,13 @@
 angular.module('myApp', [])
 .controller('MyController', function($scope) {
 $scope.task = '';
+$scope.focus = function(e) {
+  console.log(e);
+  $(".task-detail").show();
+};
+$scope.blur = function(e) {
+  console.log(e);
+  $(".task-detail").hide();
+};
+
 });
