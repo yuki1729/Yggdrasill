0998dac5d42a8760b71e2a256e0fd93f423d5c6f
igaki <gymtaka@gmail.com>
2017-02-05 16:48:50 +0900

    ワイルドカードが機能してなかったのを修正

--- routes/sessioncheck.js
+++ routes/sessioncheck.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var session = require('express-session');
 var router = express.Router();
-
-//セッションがあるかないかを判定して分岐
-router.get('/ *' , function(req, res, next) {
+console.log("セッションチェックテスト");
+//セッションがあるかないかを判定して分岐。routes以下の全部にかける方法は要検討
+router.get('/' , function(req, res, next) {
+console.log("ifの前");
   if (req.session.user_id) {
     next();
   } else {
+console.log("elseの後");
     res.redirect('/login');
     return
 
