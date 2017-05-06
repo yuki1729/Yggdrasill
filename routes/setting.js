0fe083e40dcac90a8d98ced652772cda462c37a6
t-taira-g <taira@adif-alliance.com>
2017-05-06 16:33:39 +0900

    setting情報表示
    user_id表示のみ追加

--- routes/setting.js
+++ routes/setting.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+var connection = require('../mysqlConnection'); // è¿½åŠ
+
 
 router.get('/', function(req, res, next) {
   res.render('setting',
   { title: 'setting',
-    task:'setting'
+    task:'setting',
+    test: 'hello',
+    user_id: req.session.user_id,
+    user_name: req.register_username,
+
   }
   //renderでテンプレートエンジンを指定、受け渡し数値をその中に記載
 );
