711be4182fe604a9b87cc69581e274bf3578ec44
t-taira-g <taira@adif-alliance.com>
2017-05-06 13:33:07 +0900

    Revert "setting.js setting.ejs 追加"
    This reverts commit efc7fdd79689b5182954eda1fae412568f4abf2d.

--- routes/setting.js
+++ routes/setting.js
@@ -1,10 +1,57 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+var connection = require('../mysqlConnection'); // è¿½åŠ
+var crypto = require('crypto');
+
+router.post('/', function(req, res, next) {
+  var register_username = req.body.username;
+  var register_mail = req.body.username;
+  var register_password = req.body.password;
+
+  //register_passwordの暗号化
+    var cipher = crypto.createCipher('aes-256-cbc',register_password);
+    var crypto_password = cipher.update(register_password, 'utf8', 'hex');
+    crypto_password +=  cipher.final('hex') ;
+
+  console.log(register_mail + '*=*=*=*=*=*=*=*=*');
+
+    var query = 'SELECT id FROM mydb.user WHERE user_name = "' + register_username + '" OR mail = "' + register_mail +'" AND password = "' + crypto_password + '" LIMIT 1';
+
+  /*MySQLで動くSQL文
+  SELECT * FROM mydb.user;
+  SELECT id FROM mydb.user WHERE user_name = 'igaki' OR mail = 'gymtaka@gmail.com' AND password =  '7e953ce6ce197ab31f16facdc3f403f4' LIMIT 1;
+  */
+
+//ログイン時の処理。ユーザーネームとメールアドレスの重複のチェックが不十分なので作り込む必要がある
+//三項演算子で、user_idを取得している。rows[0].idに値が入っていればtrue、そうでなければfalse
+  connection.query(query, function(err, rows) {
+    var userId = rows.length? rows[0].id: false;
+    console.log(userId + '**************');
+    if (userId) {
+      req.session.user_id = userId;
+      res.redirect('/');
+      return;
+    } else {
+      res.render('login', {
+        title: 'ログイン',
+        noUser: 'メールアドレスとパスワードが一致するユーザーはいません'
+      });
+    }
+
+
+  });
+
+});
+
+
+
+/* GET home page. */
 
 router.get('/', function(req, res, next) {
-  res.render('setting',
-  { title: 'setting',
-    task:'setting'
+  res.render('login',
+  { title: 'login',
+    task:'task'
   }
   //renderでテンプレートエンジンを指定、受け渡し数値をその中に記載
 );
@@ -12,3 +59,4 @@ router.get('/', function(req, res, next) {
 
 
 module.exports = router;
+exports.login = require('./login');
