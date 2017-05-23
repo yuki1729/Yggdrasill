42d95c3e61bbc6fa164c94e218bb6769e20dd15b
yuki1729 <yuki.o1729@gmail.com>
2017-05-24 06:44:14 +0900

    vue component作成完了

--- public/app.js
+++ public/app.js
@@ -1,43 +1,54 @@
 // 登録する
-Vue.component('addTask', {
-  template: '<div>A custom component!' +
-    '<div>B custom component!</div>'
-    + '</div>'
+//
+templateAddTask = '';
+templateAddTask += '<div>'
+templateAddTask += '<input v-model="subject" placeholder="タスク">'
+templateAddTask += '<input v-model="target" placeholder="相手先">'
+templateAddTask += '<input v-model="earliest_start_time" placeholder="開始可能日時">'
+templateAddTask += '<input v-model="deadline" placeholder="完了期限">'
+templateAddTask += '<input v-model="assigned_to_user" placeholder="担当者">'
+templateAddTask += '<button v-on:click="postTask">post</button>'
+templateAddTask += '</div>'
+
+Vue.component('my-component', {
+  template: templateAddTask,
+  data: function () {
+    return {
+    subject: "testAAA",
+    target: "2",
+    start_date: "2017/05/20",
+    deadline: "2017/05/20",
+    earliest_start_time: "2017/05/20",
+    assigned_to_user: "",
+    counter: 0
+    }
+  },
+  methods:{
+    postTask: function (event) {
+      // メソッド内の `this` は、 Vue インスタンスを参照します
+      console.log('Hello ' + this.subject + '!')
+      // `event` は、ネイティブ DOM イベントです
+      axios.post('/vue/something', {
+      subject: this.subject,
+      start_date: this.deadline,
+      actionbtn: "action",
+      assigned_to_user: 2
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    }
+  }
 })
 // root インスタンスを作成する
 new Vue({
   el: '#example'
 })
 
-var addTask = new Vue({
-	el: '#addSomething',
-	data: {
-		subject: "testAAA",
-		target: "",
-		start_date: "2017/05/20",
-		deadline: "2017/05/20",
-		earliest_start_time: "2017/05/20",
-		assigned_to_user: "",
-    counter: 0
-	},
-	methods:{
-		postTask: function (event) {
-			// メソッド内の `this` は、 Vue インスタンスを参照します
-			console.log('Hello ' + this.subject + '!')
-			// `event` は、ネイティブ DOM イベントです
-			axios.post('/vue/something', {
-			subject: this.subject,
-			start_date: this.deadline,
-			actionbtn: "action",
-			assigned_to_user: 2
-	  })
-	  .then(function (response) {
-	    console.log(response);
-	  })
-	  .catch(function (error) {
-	    console.log(error);
-	  });
-
-		}
-	}
-});
+new Vue({
+  el: '#example2'
+})
