ccea798771e7265e6b211bc54f4544a57f683768
t-taira-g <taira@adif-alliance.com>
2017-06-03 20:59:42 +0900

    vueテスト中断
    vueテスト中断

--- public/js/vue.js
+++ public/js/vue.js
@@ -4,3 +4,23 @@ new Vue({
     message: 'Hello Vue.js!'
   }
 })
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: '123456789'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var userdata = new Vue({
+  el: '#user-data',
+  data: {
+    user_id: 'testid',
+    user_name: 'testname',
+    user_mail: 'testmail'
+  }
+})
