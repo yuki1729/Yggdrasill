2ebd4b6b268af3c9b082736fb141cbf4d4086f0f
yuki1729 <yuki.o1729@gmail.com>
2017-06-18 16:07:16 +0900

    不要コード除去

--- public/app.js
+++ public/app.js
@@ -10,24 +10,6 @@ temp =
 		<li><input v-model="earliest_start_time" class="form-control" placeholder="開始可能日時"></li>
 		<li><input v-model="deadline" class="form-control" placeholder="完了期限"></li><br>
 	</ul>
-
-	<div class="form-group">
-		<div class="col-sm-offset-2 col-sm-10">
-			<label class="control-label">繰り返し設定</label><br>
-			<input type="radio" name="毎月" value="monthly">毎月
-			<input type="radio" name="毎週" value="weekly">毎週
-			<input type="radio" name="毎日" value="dayily">毎日
-			<div class="checkbox">
-				<label><input type="checkbox" value="checkbox1"> 月曜</label>
-				<label><input type="checkbox" value="checkbox2"> 火曜</label>
-				<label><input type="checkbox" value="checkbox3"> 水曜</label>
-				<label><input type="checkbox" value="checkbox4"> 木曜</label>
-				<label><input type="checkbox" value="checkbox5"> 金曜</label>
-				<label><input type="checkbox" value="checkbox6"> 土曜</label>
-				<label><input type="checkbox" value="checkbox7"> 日曜</label>
-			</div>
-		</div>
-	</div>
 	<ul class="form-group">
 		<input v-model="assigned_to_user" placeholder="担当者">
 		<div class="form-group">
@@ -42,12 +24,12 @@ temp =
 Vue.component('my-component', {
 	template: temp,
 	data: function() {
-		var test = "dev" + moment().format('MMMDo h:mm:ss');
+		var subject = "dev" + moment().format('MMMDo h:mm:ss');
 		var now = moment();
 		var deadline = moment({ hour:17, minute:00 });
 
 		return {
-			subject: test,
+			subject: subject,
 			target: "井垣さん",
 			earliest_start_time: now.format('MMMDo h:mm:ss'),
 			deadline: deadline.format('kk:mm'),
@@ -87,3 +69,24 @@ Vue.component('my-component', {
 new Vue({
 	el: '#example'
 })
+
+
+/* α版では使用しない
+<div class="form-group">
+		<div class="col-sm-offset-2 col-sm-10">
+			<label class="control-label">繰り返し設定</label><br>
+			<input type="radio" name="毎月" value="monthly">毎月
+			<input type="radio" name="毎週" value="weekly">毎週
+			<input type="radio" name="毎日" value="dayily">毎日
+			<div class="checkbox">
+				<label><input type="checkbox" value="checkbox1"> 月曜</label>
+				<label><input type="checkbox" value="checkbox2"> 火曜</label>
+				<label><input type="checkbox" value="checkbox3"> 水曜</label>
+				<label><input type="checkbox" value="checkbox4"> 木曜</label>
+				<label><input type="checkbox" value="checkbox5"> 金曜</label>
+				<label><input type="checkbox" value="checkbox6"> 土曜</label>
+				<label><input type="checkbox" value="checkbox7"> 日曜</label>
+			</div>
+		</div>
+	</div>
+*/
