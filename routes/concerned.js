410748e3519ecd3bcf09e9ff4c195ed8bfd81097
0ya1sehara <s.focsoup@gmail.com>
2017-07-17 11:28:13 +0900

    test
    test

---
+++ routes/concerned.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var moment = require('moment');
+var connection = require('../mysqlConnection'); // è¿½åŠ
+
+/* GETリクエストを受けた時の処理 */
+
+router.get('/', function(req, res, next) {
+  res.render('concerned',
+  { title: '関係者のメンション設定',
+  }
+  //renderでテンプレートエンジンを指定、受け渡し数値をその中に記載
+);
+});
+
+router.post('/', function(req, res, next) {
+  var nickname = '"' + req.body.nickname + '"';
+  // var mension = '"' + req.body.mension + '"';
+  console.log("----------concernd post-------------------")
+  console.log(req.body)
+  var query =
+    'INSERT INTO concerned (nickname,self_user_id,partner_user_id) VALUES ('
+    + nickname
+    + 2
+    + 2
+    + ')';
+    var post_value = {
+      nickname: req.body.nickname,
+      at_name: "@name",
+      self_user_id: 2,
+      partner_user_id: 2
+    };
+
+
+  var query = connection.query('INSERT INTO concerned SET ?', post_value, function(err, rows) {
+    res.redirect('/concerned');
+  });
+});
+
+// test
+
+module.exports = router;
+exports.concerned = require('./concerned');
