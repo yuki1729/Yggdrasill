ab025ce4cce827673a4f8ab065cbcc4d63f12915
Takayuki Igaki <gymtaka@gmail.com>
2017-08-19 11:45:11 +0900

    中断

--- routes/vuetest.js
+++ routes/vuetest.js
@@ -5,19 +5,19 @@ var connection = require('../mysqlConnection');
 
 router.get('/', function(req, res, next) {
   // 割当先ユーザーIDを取得
-  var query = 'SELECT *, DATE_FORMAT(start_date, \'%Y年%m月%d日 %k時%i分%s秒\') AS start_date, DATE_FORMAT(finish_date, \'%Y年%m月%d日 %k時%i分%s秒\') AS finish_date FROM something inner join assignment_relation on something.id = assignment_relation.something_id';
-  getQuery = connection.query(query, function(err, rows) {
-    console.log(getQuery + "*********************");
-/*
+
     res.render('/', {
-      taskList: rows
+
     });
-  */
-  });
+
   // console.log(sqlQuery.sql);
 });
 
 router.post('/getquery', function(req, res, next) {
+  var query = 'SELECT *, DATE_FORMAT(start_date, \'%Y年%m月%d日 %k時%i分%s秒\') AS start_date, DATE_FORMAT(finish_date, \'%Y年%m月%d日 %k時%i分%s秒\') AS finish_date FROM something inner join assignment_relation on something.id = assignment_relation.something_id';
+  getQuery = connection.query(query, function(err, rows) {
+    console.log(getQuery + "*********************");
+      });
 /*
   if (debug){ //デバッグモード時はログインしていなくてもid1でログインしたことにする
     if (req.session.user_id == null ) req.session.user_id =1;
