fcd2018d4bf7f3649b1270d919c716205223ad4c
yuki1729 <yuki.o1729@gmail.com>
2017-09-02 10:24:37 +0900

    不要コード削除

--- public/js/user-suggest.js
+++ public/js/user-suggest.js
@@ -4,59 +4,34 @@ element_template =
 		<li><input v-model="userList" class="form-control" placeholder="タスク"></li>
 	</ul><p>{{userList}}</p>
 </form>`;
-var user1 = "test user";
+
 Vue.component('user-suggest', {
 	template: element_template,
 	data: function() {
 		return{
-			 userList : "init	"
+			 userList : ""
 		}
 	},
 	methods: {
 		loadUserList: function(){
-			console.log("before axios");
-			var self = this;
+			var self = this; // axiosのthen内でこのvue componentにアクセスするためthisを代入する。
 			axios.get('/vue_test/userList', {
+					// request parameter
 				})
 				.then(function(response) {
-					console.log("get response data");
-					console.log(response.data[0].user_name);
-					console.log(this);
 					self.userList = response.data[0].user_name;
-					// window.foo = "bar";
-					// console.log(this);
-					// console.log(this.foo);
-					//user1 = response.data[0].user_name;
-					// console.log(vc);
-					// this.userList.$set("test") //response.data[0].user_name;
-					//this.$set(this.data, "userList",response.data[0].user_name);
-					// this.$forceUpdate();
 				})
 				.catch(function(error) {
 					console.log(error);
 				});
-
-		},
-		test:function(){
-			console.log("test function");
 		}
 	},
 	created: function(){
-		console.log("create user list");
+		// vue component 生成時に実行
 		this.loadUserList();
-		this.userList = "created"
-		console.log("created log ");
-		console.dir(this);
-	},
-	mounted: function () {
-		console.log("mounted");
-		this.userList = "mounted"
-		this.test();
 	}
 })
 
 var vc = new Vue({
 	el: '#suggest'
 })
-//vc.userList = "test";
-//Vue.set(vc.$data,"useerList","aaa")
